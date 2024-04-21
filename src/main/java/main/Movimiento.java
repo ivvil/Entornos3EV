f703daf8cf46231cd529bd7b63f4016906ea9495
Iván Villagrasa <ivvil412@gmail.com>
2024-04-22 01:07:43 +0200

    AmonGus

--- src/main/java/main/Movimiento.java
+++ src/main/java/main/Movimiento.java
@@ -218,7 +218,7 @@ Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci comm
 	
 	Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.	
 	 */
-	private static int numSig = 1;
+    public static int numSig = 1;
 
 	/**
 	 * Construye un nuevo objeto {@link Movimiento}, probando que ni la {@code fecha} ni la {@code cuenta}
